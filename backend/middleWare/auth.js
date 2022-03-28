0d83d5340de05ccb4bb9fce90bef80661c2e3b9c
ericerac <eric2022contact@gmail.com>
2022-03-28 23:28:07 +0200

    bruteForce

--- backend/middleWare/auth.js
+++ backend/middleWare/auth.js
@@ -1,21 +1,22 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   try {
-      console.log("on est là auth");
-      //console.log(userId);
+    console.log("on est là auth");
+    //console.log(userId);
 
-    const token = req.headers.authorization.split(' ')[1]; // récupère le token dans le header
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); // décrypte le token
+    const token = req.headers.authorization.split(" ")[1]; // récupère le token dans le header
+    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET"); // décrypte le token
     const userId = decodedToken.userId; // récupère l'id du token
-    if (req.body.userId && req.body.userId !== userId) { // compare l'id du token avec l'id utilisateur
-      throw 'Invalid user ID';
+    if (req.body.userId && req.body.userId !== userId) {
+      // compare l'id du token avec l'id utilisateur
+      throw "Invalid user ID";
     } else {
-      next();  // si Id identhique passe au middleware suivant " la route est protégée"
+      next(); // si Id identhique passe au middleware suivant " la route est protégée"
     }
   } catch {
     res.status(401).json({
-      error: new Error('Invalid request!')
+      error: new Error("Invalid request!"),
     });
   }
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,14 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userControl = require('../controllers/user')
-const passWord = require('../middleWare/passValidator.js')
+const userControl = require("../controllers/user");
+const passWord = require("../middleWare/passValidator.js");
+const ExpressBrute = require('express-brute');
+const store = new ExpressBrute.MemoryStore(); // stores state locally, don't use this in production
+const bruteforce = new ExpressBrute(store);
 
-// const multer = require('../middleWare/multer-config.js');
-
-
-router.post('/signup', passWord, userControl.signup);
-router.post('/login', userControl.login);
-
-
+router.post("/signup", passWord, userControl.signup);
+//router.post("/login", userControl.login);
 
 module.exports = router;
+
+
+router.post('/login',
+    bruteforce.prevent, userControl.login, // error 429 if we hit this route too often
+    function (req, res, next) {
+        res.send('Success!');
+    }
+);
+
