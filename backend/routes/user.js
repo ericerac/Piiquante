17c1da8b8b03d201988077c8bc3bf5ff5902a814
ericerac <eric2022contact@gmail.com>
2022-03-28 23:33:10 +0200

    force

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,20 +2,20 @@ const express = require("express");
 const router = express.Router();
 const userControl = require("../controllers/user");
 const passWord = require("../middleWare/passValidator.js");
-const ExpressBrute = require('express-brute');
-const store = new ExpressBrute.MemoryStore(); // stores state locally, don't use this in production
+const ExpressBrute = require("express-brute");
+const store = new ExpressBrute.MemoryStore();
 const bruteforce = new ExpressBrute(store);
 
 router.post("/signup", passWord, userControl.signup);
 //router.post("/login", userControl.login);
 
-module.exports = router;
-
-
-router.post('/login',
-    bruteforce.prevent, userControl.login, // error 429 if we hit this route too often
-    function (req, res, next) {
-        res.send('Success!');
-    }
+router.post(
+  "/login",
+  bruteforce.prevent,
+  userControl.login, // error 429 if we hit this route too often
+  function (req, res, next) {
+    res.send("Success!");
+  }
 );
 
+module.exports = router;
