66ef83278f5472372f5d76bf443c96499ab2fc9b
ericerac <eric2022contact@gmail.com>
2022-03-31 10:51:48 +0200

    comment

--- backend/app.js
+++ backend/app.js
@@ -1,54 +1,50 @@
-const dotenv = require('dotenv');
-dotenv.config({path:'./.env'});
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
 
-const mongoose = require('mongoose');
-const mongoSanitize = require('express-mongo-sanitize');
+const mongoose = require("mongoose");
+const mongoSanitize = require("express-mongo-sanitize");
 
-
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 
 app.use(mongoSanitize());
 
-const sauceRoutes = require('./routes/sauces');
-const userRoutes = require('./routes/user');
+const sauceRoutes = require("./routes/sauces");
+const userRoutes = require("./routes/user");
 
-const cors = require('cors');
-const uuid4 = require('uuid').v4;
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.quq8c.mongodb.net/${process.env.DB_NAME_TAB}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
 
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch((err) => console.log("Connexion à MongoDB échouée !", err));
 
-var corsOptions = {
-    origin: 'http://localhost:8081',
-    optionsSuccessStatus: 200 
-  }
-// connection to mongoDB
-
-mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.quq8c.mongodb.net/${process.env.DB_NAME_TAB}?retryWrites=true&w=majority`,
-    {   
-    useNewUrlParser: true,
-    useUnifiedTopology: true })
-
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch((err) => console.log('Connexion à MongoDB échouée !',(err)));
-
-  
-
-  // cors
+// cors
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 //app.use(cors(corsOptions));
 
-app.use('/images', express.static('images'));
-app.use(express.static('images'));
+app.use("/images", express.static("images"));
+app.use(express.static("images"));
 
-
-app.use('/api/sauces', sauceRoutes);
-app.use('/api/auth', userRoutes) ;
+app.use("/api/sauces", sauceRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
